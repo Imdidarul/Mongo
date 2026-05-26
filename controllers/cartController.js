0b37b935298673b07e941f45ca76784a697cdad6
Didarul Islam Laskar <95709968+Imdidarul@users.noreply.github.com>
2026-05-26 16:19:03 +0530

    Used mongoose in cart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,86 +1,93 @@
 const Cart = require("../models/cart")
 
-const addCartItem = (req,res)=>{
-    const {product, quantity, userId} = req.body
 
-    const cartItem = new Cart(product, quantity, userId)
+const addCartItem = async (req,res)=>{
+    try {
+        const {product, quantity, userId} = req.body
 
-    cartItem.save().then(()=>{
-        console.log("Added successfully")
-        res.status(200).send("Item added to cart")
-    }).catch(err=>{
-        console.log(err)
-        res.status(500).send("Server failure")
-    })
-}
-
-
-const findByuserId = (req,res) =>{
-    const {id} = req.query
-    const cartItems = Cart.findbyId(id).then(result=>{
-        // console.log("Cart items retrieved")
-        // console.log(result)
-        res.status(200).send(result)
-    }).catch(err=>{
-        console.log(err)
+        const cartItem = new Cart({product, quantity, userId})
+    
+        const cart = await cartItem.save()
+        res.status(201).send("Item added")
+    } catch (error) {
+        console.log(error)
         res.status(500).send("Server side error")
-    })
+    }
+
 }
 
-// const findAll = (req,res)=>{
-//     Product.findAll().then(result=>{
-//         console.log(result)
-//         res.status(200).send(result)
-//     }).catch(err=>{
-//         console.log(err)
-//         res.status(500).send("Serverside error")
-//     })
-// }
 
-const deleteById = (req,res)=>{
-    const {id} = req.query
-    const deletedCount = Cart.deleteById(id).then(result=>{
-        // console.log(result)
-        res.status(200).send(result)
-    }).catch(err=>{
-        console.log(err)
-        res.status(500).send("Serverside error")
-    })
-}
-
-const updateById = (req,res)=>{
-    const {id} = req.query
-
-    const data = req.body
-    // console.log(data)
-
-    Cart.updateById(id,data).then(result=>{
-        console.log(result.matchedCount)
-        res.status(200).send(result)
-    }).catch(err=>{
-        console.log(err)
+const findByuserId = async (req,res) =>{
+    try {
+        const {id} = req.query
+        const cartItems = await Cart.find({userId:id}).exec()
+        if (!cartItems){
+            return res.status(404).send("Could not fetch cart items")
+        }
+    
+        res.status(200).send(cartItems)
+    } catch (error) {
+        console.log(error)
         res.status(500).send("Server side error")
-    })
+    }
+}
+
+
+const deleteById = async (req,res)=>{
+    try {
+        const {id} = req.query
+        const deletedCount = await Cart.deleteOne({_id:id}).exec()
+        if(!deletedCount){
+            return res.status(404).send("Could not be deleted")
+        }
+        res.status(200).send("Deleted successfully")
+    } catch (error) {
+        console.log(error)
+        res.status(500).send("Server side error")
+    }
+}
+
+
+const updateById = async (req,res)=>{
+    try {
+        const {id} = req.query
+
+        const data = req.body
+    
+        const updated = await Cart.updateOne({_id:id},data)
+        if(!updated){
+            return res.status(404).send("Could not be updated")
+        }
+    
+        res.staus(200).send("Updated successfully")
+    } catch (error) {
+        console.log(error)
+        res.status(500).send("Server side error")
+    }
 }
 
 
 const increaseCartItemQuantity = async (req,res)=>{
-    const {id} = req.query
+    try {
+        const {id} = req.query
 
-    const cartItem = await Cart.findItembyId(id)
-    // console.log(cartItem)
-
-    const quantity = cartItem.quantity+1
-
-    const data = {quantity:quantity}
-    Cart.updateById(id,data).then(result=>{
-        console.log(result.matchedCount)
-        res.status(200).send(result.matchedCount)
-    }).catch(err=>{
-        console.log(err)
+        const cartItem = await Cart.findOne({_id:id})
+    
+        const quantity = cartItem.quantity+1
+    
+        const data = {quantity:quantity}
+    
+        const updated = await Cart.updateOne({_id:id},data)
+        if(!updated){
+            return res.status(404).send("Couldnot be updated")
+        }
+        res.status(200).send("Updated sucessfully")
+    } catch (error) {
+        console.log(error)
         res.status(500).send("Server side error")
-    })
+    }
 }
+
 
 
 
@@ -88,21 +95,19 @@ const decreaseCartItemQuantity = async(req,res)=>{
     try {
         const {id} = req.query
 
-        const cartItem = await Cart.findItembyId(id)
+        const cartItem = await Cart.findOne({_id:id})
     
-        // console.log("Quantity is:-",cartItem.quantity)
     
         const quantity = cartItem.quantity-1
     
         if (quantity == 0){
-            const result = await Cart.deleteById(id)
-                // console.log(result)
-                console.log("Item deleted as quantity reached 0")
-                return res.status(200).send("Quantity became zero so deleted item from cart")
-            }
+            const result = await Cart.deleteOne({_id:id})
+            console.log("Item deleted as quantity reached 0")
+            return res.status(200).send("Item removed from cart")
+        }
     
         const data = {quantity:quantity}
-        const result = await Cart.updateById(id,data)
+        const result = await Cart.updateOne({_id:id},data)
             console.log(result.matchedCount)
             res.status(200).send(result.matchedCount)
         
@@ -113,5 +118,120 @@ const decreaseCartItemQuantity = async(req,res)=>{
 
 }
 
-
 module.exports = {addCartItem,findByuserId, deleteById, updateById, increaseCartItemQuantity, decreaseCartItemQuantity}
+
+// const addCartItem = (req,res)=>{
+//     const {product, quantity, userId} = req.body
+
+//     const cartItem = new Cart(product, quantity, userId)
+
+//     cartItem.save().then(()=>{
+//         console.log("Added successfully")
+//         res.status(200).send("Item added to cart")
+//     }).catch(err=>{
+//         console.log(err)
+//         res.status(500).send("Server failure")
+//     })
+// }
+
+
+// const findByuserId = (req,res) =>{
+//     const {id} = req.query
+//     const cartItems = Cart.findbyId(id).then(result=>{
+//         // console.log("Cart items retrieved")
+//         // console.log(result)
+//         res.status(200).send(result)
+//     }).catch(err=>{
+//         console.log(err)
+//         res.status(500).send("Server side error")
+//     })
+// }
+
+// // const findAll = (req,res)=>{
+// //     Product.findAll().then(result=>{
+// //         console.log(result)
+// //         res.status(200).send(result)
+// //     }).catch(err=>{
+// //         console.log(err)
+// //         res.status(500).send("Serverside error")
+// //     })
+// // }
+
+// const deleteById = (req,res)=>{
+//     const {id} = req.query
+//     const deletedCount = Cart.deleteById(id).then(result=>{
+//         // console.log(result)
+//         res.status(200).send(result)
+//     }).catch(err=>{
+//         console.log(err)
+//         res.status(500).send("Serverside error")
+//     })
+// }
+
+// const updateById = (req,res)=>{
+//     const {id} = req.query
+
+//     const data = req.body
+//     // console.log(data)
+
+//     Cart.updateById(id,data).then(result=>{
+//         console.log(result.matchedCount)
+//         res.status(200).send(result)
+//     }).catch(err=>{
+//         console.log(err)
+//         res.status(500).send("Server side error")
+//     })
+// }
+
+
+// const increaseCartItemQuantity = async (req,res)=>{
+//     const {id} = req.query
+
+//     const cartItem = await Cart.findItembyId(id)
+//     // console.log(cartItem)
+
+//     const quantity = cartItem.quantity+1
+
+//     const data = {quantity:quantity}
+//     Cart.updateById(id,data).then(result=>{
+//         console.log(result.matchedCount)
+//         res.status(200).send(result.matchedCount)
+//     }).catch(err=>{
+//         console.log(err)
+//         res.status(500).send("Server side error")
+//     })
+// }
+
+
+
+// const decreaseCartItemQuantity = async(req,res)=>{
+//     try {
+//         const {id} = req.query
+
+//         const cartItem = await Cart.findItembyId(id)
+    
+//         // console.log("Quantity is:-",cartItem.quantity)
+    
+//         const quantity = cartItem.quantity-1
+    
+//         if (quantity == 0){
+//             const result = await Cart.deleteById(id)
+//                 // console.log(result)
+//                 console.log("Item deleted as quantity reached 0")
+//                 return res.status(200).send("Quantity became zero so deleted item from cart")
+//             }
+    
+//         const data = {quantity:quantity}
+//         const result = await Cart.updateById(id,data)
+//             console.log(result.matchedCount)
+//             res.status(200).send(result.matchedCount)
+        
+//     } catch (error) {
+//         console.log(error)
+//         res.status(500).send("Server side error")   
+//     }
+
+// }
+
+
+// module.exports = {addCartItem,findByuserId, deleteById, updateById, increaseCartItemQuantity, decreaseCartItemQuantity}
--- models/cart.js
+++ models/cart.js
@@ -1,74 +1,95 @@
-const mongodb = require("mongodb")
-const {getDb} = require("../utils/database")
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
 
-class Cart {
-    constructor(product,quantity,userId){
-        this.product = product;
-        this.quantity = quantity;
-        this.userId = userId
+const cartSchema = new Schema({
+    product:{
+        type: Object,
+        required: true
+    },
+    quantity:{
+        type: Number,
+        required: true
+    },
+    userId:{
+        type: String,
+        required: true
     }
+})
 
-    save(){
-        const db = getDb()
-        return db.collection('cart').insertOne(this)
-        .then( result =>{
-            console.log(result)
-        }).catch((err)=>{
-            console.log(err)
-            throw err
-        })
-    }
-    static findbyId(userId){
-        const db = getDb()
-        return db.collection('cart').find({userId: userId}).toArray().then((cart)=>{
-            console.log(cart)
-            return cart
-        }).catch(err=>{
-            console.log(err)
-            throw err
-        })
-    }
+module.exports = mongoose.model("Cart", cartSchema)
 
-    static findItembyId(id){
-        const db = getDb()
-        return db.collection('cart').find({_id: new mongodb.ObjectId(id)}).next().then((result)=>{
-            console.log(result)
-            return result
-        }).catch(err=>{
-            console.log(err)
-            throw err
-        })
-    }
 
-    static deleteById(cartId){
-        const db = getDb()
+// const mongodb = require("mongodb")
+// const {getDb} = require("../utils/database")
 
-        return db.collection('cart').deleteMany({_id: new mongodb.ObjectId(cartId)}).then(result=>{
-            console.log(result.deletedCount)
-            return result.deletedCount
-        }).catch(err=>{
-            console.log(err)
-            throw err
-        })
-    }
+// class Cart {
+//     constructor(product,quantity,userId){
+//         this.product = product;
+//         this.quantity = quantity;
+//         this.userId = userId
+//     }
 
-    static updateById(id, data){
-        const db = getDb()
-        // const updatedDoc = doc
-        console.log(data)
-        return db.collection("cart").updateOne(
-            {_id:new mongodb.ObjectId(id)},
-            {
-                $set:data,
-                $currentDate:{lastModified: true}
-            }
-        ).then(result=>{
-            console.log("Updated")
-            return result
-        }).catch(err=>{
-            // console.log(err)
-            throw err
-        })
-    }
-}
-module.exports = Cart
+//     save(){
+//         const db = getDb()
+//         return db.collection('cart').insertOne(this)
+//         .then( result =>{
+//             console.log(result)
+//         }).catch((err)=>{
+//             console.log(err)
+//             throw err
+//         })
+//     }
+//     static findbyId(userId){
+//         const db = getDb()
+//         return db.collection('cart').find({userId: userId}).toArray().then((cart)=>{
+//             console.log(cart)
+//             return cart
+//         }).catch(err=>{
+//             console.log(err)
+//             throw err
+//         })
+//     }
+
+//     static findItembyId(id){
+//         const db = getDb()
+//         return db.collection('cart').find({_id: new mongodb.ObjectId(id)}).next().then((result)=>{
+//             console.log(result)
+//             return result
+//         }).catch(err=>{
+//             console.log(err)
+//             throw err
+//         })
+//     }
+
+//     static deleteById(cartId){
+//         const db = getDb()
+
+//         return db.collection('cart').deleteMany({_id: new mongodb.ObjectId(cartId)}).then(result=>{
+//             console.log(result.deletedCount)
+//             return result.deletedCount
+//         }).catch(err=>{
+//             console.log(err)
+//             throw err
+//         })
+//     }
+
+//     static updateById(id, data){
+//         const db = getDb()
+//         // const updatedDoc = doc
+//         console.log(data)
+//         return db.collection("cart").updateOne(
+//             {_id:new mongodb.ObjectId(id)},
+//             {
+//                 $set:data,
+//                 $currentDate:{lastModified: true}
+//             }
+//         ).then(result=>{
+//             console.log("Updated")
+//             return result
+//         }).catch(err=>{
+//             // console.log(err)
+//             throw err
+//         })
+//     }
+// }
+// module.exports = Cart
